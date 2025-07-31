f7c487d2c9d36998f0645c70772e29d5b5a4543a
sachi1010 <131956987+sachi1010@users.noreply.github.com>
2025-07-31 12:20:28 +0530

    Update Hero.jsx

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div className='hero' id='home'>
       <div className="hero-img-container">
         <div className="glow-background" ></div>
-        <div className='hero-image' data-aos="zoom-in"><img src="http://localhost:5000/public/heroimg.png" alt="Profile" /></div>  
+        <div className='hero-image' data-aos="zoom-in"><img src="https://my-portfolio-backend-k3s2.onrender.com/public/heroimg.png" alt="Profile" /></div>  
       </div>
       <h1 data-aos="zoom-in"><span>I'm Sachithananthan</span><br />Full Stack Developer</h1>
       <p data-aos="zoom-in">I'm a dedicated Full Stack Developer with a strong interest in building responsive web applications.</p>
@@ -16,7 +16,7 @@ const Hero = () => {
         <div className="hero-connect">
           <AnchorLink className='ancher-link' offset={50} href='#contact'>Connect With Me</AnchorLink>
         </div>
-        <a href="http://localhost:5000/api/resume" download target="_blank" rel="noopener noreferrer" className='hero-resume'>
+        <a href="https://my-portfolio-backend-k3s2.onrender.com/api/resume" download target="_blank" rel="noopener noreferrer" className='hero-resume'>
     My Resume
   </a>
       </div>
