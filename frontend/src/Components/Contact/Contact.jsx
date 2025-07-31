c7219e81e144de25652854ad901d7ea5729ad5df
sachi1010 <131956987+sachi1010@users.noreply.github.com>
2025-07-31 12:19:13 +0530

    Update Contact.jsx

--- frontend/src/Components/Contact/Contact.jsx
+++ frontend/src/Components/Contact/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
   };
 
   try {
-    const res = await fetch("http://localhost:5000/api/contact", {
+    const res = await fetch("https://my-portfolio-backend-k3s2.onrender.com/api/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
